671b005181d87837acb9ecb23c0f850fbd4a9aa2
Alones <polbcc167@gmail.com>
2023-03-16 14:27:55 +0700

    Appts CRUD API

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -1,4 +1,5 @@
 const Appointment = require("../models/Appointment");
+const Hospital = require("../models/Hospital");
 
 //@desc Get all appointments
 //@route GET /api/v1/appointments
@@ -27,5 +28,151 @@ exports.getAppointments = async (req, res, next) => {
     res
       .status(500)
       .json({ success: false, message: "Cannot find Appointment" });
+  }
+};
+
+//@desc Get single appointment
+//@route GET /api/v1/appointments/:id
+//@access Public
+exports.getAppointment = async (req, res, next) => {
+  try {
+    const appointment = await Appointment.findById(req.params.id).populate({
+      path: "hospital",
+      select: "name description tel",
+    });
+
+    if (!appointment) {
+      return res.status(404).json({
+        success: false,
+        message: `No appointment with the id of ${req.params.id}`,
+      });
+    }
+
+    res.status(200).json({ success: true, data: appointment });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "Cannot find Appointment" });
+  }
+};
+
+//@desc Add appointment
+//@route POST /api/v1/appointments
+//@access Private
+exports.addAppointment = async (req, res, next) => {
+  try {
+    //Add user and hospital to req.body
+    req.body.user = req.user.id;
+    req.body.hospital = req.params.hospitalId;
+
+    //Check for existed appointment
+    const existedAppointment = await Appointment.find({
+      user: req.user.id,
+      hospital: req.params.hospitalId,
+    });
+
+    //If the user is not an admin, they can only create 3 appointments
+    if (existedAppointment.length >= 3 && req.user.role !== "admin") {
+      return res.status(400).json({
+        success: false,
+        message: `The user with ID ${req.user.id} has already made 3 appointments`,
+      });
+    }
+
+    const hospital = await Hospital.findById(req.params.hospitalId);
+
+    if (!hospital) {
+      return res.status(404).json({
+        success: false,
+        message: `No hospital with the id of ${req.params.hospitalId}`,
+      });
+    }
+
+    const appointment = await Appointment.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: appointment,
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ success: false, message: "Cannot create Appointment" });
+  }
+};
+
+//@desc Update appointment
+//@route PUT /api/v1/appointments/:id
+//@access Private
+exports.updateAppointment = async (req, res, next) => {
+  try {
+    let appointment = await Appointment.findById(req.params.id);
+
+    // Make sure user is the appointment owner
+    if (
+      appointment.user.toString() !== req.user.id &&
+      req.user.role !== "admin"
+    ) {
+      return res.status(401).json({
+        success: false,
+        message: `User ${req.user.id} is not authorized to update this appointment`,
+      });
+    }
+
+    if (!appointment) {
+      return res.status(404).json({
+        success: false,
+        message: `No appointment with the id of ${req.params.id}`,
+      });
+    }
+
+    appointment = await Appointment.findOneAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({ success: true, data: appointment });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ success: false, message: "Cannot update Appointment" });
+  }
+};
+
+//@desc Delete appointment
+//@route DELETE /api/v1/appointments/:id
+//@access Private
+exports.deleteAppointment = async (req, res, next) => {
+  try {
+    const appointment = await Appointment.findById(req.params.id);
+
+    // Make sure user is the appointment owner
+    if (
+      appointment.user.toString() !== req.user.id &&
+      req.user.role !== "admin"
+    ) {
+      return res.status(401).json({
+        success: false,
+        message: `User ${req.user.id} is not authorized to update this appointment`,
+      });
+    }
+
+    if (!appointment) {
+      return res.status(404).json({
+        success: false,
+        message: `No appointment with the id of ${req.params.id}`,
+      });
+    }
+
+    await appointment.remove();
+
+    res.status(200).json({ success: true, data: {} });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ success: false, message: "Cannot delete Appointment" });
   }
 };
--- routes/appointments.js
+++ routes/appointments.js
@@ -1,11 +1,26 @@
 const express = require("express");
 
-const { getAppointments } = require("../controllers/appointments");
+const {
+  getAppointments,
+  getAppointment,
+  addAppointment,
+  updateAppointment,
+  deleteAppointment,
+} = require("../controllers/appointments");
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
-const { protect } = require("../middleware/auth");
+const { protect, authorize } = require("../middleware/auth");
 
-router.route("/").get(protect, getAppointments);
+router
+  .route("/")
+  .get(protect, getAppointments)
+  .post(protect, authorize("admin", "user"), addAppointment);
+
+router
+  .route("/:id")
+  .get(protect, getAppointment)
+  .put(protect, authorize("admin", "user"), updateAppointment)
+  .delete(protect, authorize("admin", "user"), deleteAppointment);
 
 module.exports = router;
